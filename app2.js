3cfd8c8004eb1cbb823e985ff7ff65dd938c6e22
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 18:50:44 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,6 @@
+let pencilPrice = 10;
+let erasorPrice = 5;
+// let output = 
+// "The total price is : " + (pencilPrice + erasorPrice) + " Rupees.";
+let output  = `The total price is : ${pencilPrice + erasorPrice } Rupees,`; //back ticks
+console.log(`The total price is : ${pencilPrice + erasorPrice } Rupees.`);
